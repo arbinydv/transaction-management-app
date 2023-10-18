16678d4df1a852cd4c9edff2d77ae858264c951e
arbinydv <sominyadav7@gmail.com>
2023-10-18 17:54:44 +0545

    Create modal to view transaction detail

--- client/src/components/TransactionDetail.tsx
+++ client/src/components/TransactionDetail.tsx
@@ -6,14 +6,18 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
-  Button,
   Text,
-  Flex,
-  HStack,
   Icon,
-  Link,
+  Input,
+  Stack,
+  Flex,
 } from "@chakra-ui/react";
-import { FiPaperclip } from 'react-icons/fi';
+import {
+  GoCircle,
+  GoLocation,
+  GoPaperclip,
+  GoPlus,
+} from "react-icons/go";
 
 interface TransactionDetailProps {
   isOpen: boolean;
@@ -21,49 +25,90 @@ interface TransactionDetailProps {
   selectedTransaction: Transaction | null;
 }
 
-function TransactionDetail({ isOpen, onClose, selectedTransaction }: TransactionDetailProps) {
+function TransactionDetail({
+  isOpen,
+  onClose,
+  selectedTransaction,
+}: TransactionDetailProps) {
   if (!selectedTransaction) {
     return null;
   }
 
-  function formatDate(inputDate: string) {
+  function formatDateTime(inputDate) {
     const date = new Date(inputDate);
-    const options = { month: 'short', day: 'numeric' };
-    return date.toLocaleDateString('en-US', options);
+
+    // Format the date (e.g., "Oct 10")
+    const dateOptions = { month: 'short', day: 'numeric' };
+    const formattedDate = date.toLocaleDateString('en-US', dateOptions);
+
+    // Format the time (e.g., "04:35 PM")
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    const formattedTime =
+      (hours % 12 || 12) +
+      ':' +
+      (minutes < 10 ? '0' : '') +
+      minutes +
+      ' ' +
+      ampm;
+    return `${formattedDate}, ${formattedTime}`;
   }
 
+  const amountStyle = {
+    fontSize: "40px",
+    fontWeight: "bold",
+    fontFamily: "monospace",
+  };
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} size="lg">
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Transaction Details</ModalHeader>
+        <ModalHeader fontFamily="monospace" fontSize="16px" fontWeight="light">
+          {selectedTransaction.paymentMethod}
+        </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Flex justify="space-between" align="center" mb="2">
-            <Text>
-              <HStack spacing={1}>
-                <Icon as={FiPaperclip} />
-                {formatDate(selectedTransaction.date)}
-              </HStack>
+          <Stack spacing={4}>
+            <Flex align="center" alignItems="center">
+              <Text style={amountStyle}>{selectedTransaction.amount}</Text>
+            </Flex>
+            <Text fontWeight="bold" fontSize="lg" fontFamily="monospace">
+              <Icon as={GoCircle} boxSize={4} color="gray.500" mr={2} fontSize="lg" />
+              From {selectedTransaction.sender}
             </Text>
-            {selectedTransaction.attachment && (
-              <Link href={selectedTransaction.attachment} target="_blank">
-                <HStack spacing={1}>
-                  <Icon as={FiPaperclip} />
-                  View Attachment
-                </HStack>
-              </Link>
-            )}
-          </Flex>
-          <Text>Amount: {selectedTransaction.amount}</Text>
-          <Text>From: {selectedTransaction.sender}</Text>
-          <Text>To: {selectedTransaction.receiver}</Text>
-          <Text>Payment Method: {selectedTransaction.paymentMethod}</Text>
+            <Flex direction="column" mx="6">
+              <span>{selectedTransaction.account}</span>
+              <span>{formatDateTime(selectedTransaction.date)}</span>
+            </Flex>
+            <Text fontWeight="bold" fontSize="lg" fontFamily="monospace">
+              <Icon as={GoLocation} boxSize={4} color="blue.500" mr={2} fontSize="lg" />
+              To {selectedTransaction.receiver}
+            </Text>
+            <Flex direction="column" mx="6">
+              <span>{selectedTransaction.account}</span>
+              <span>{formatDateTime(selectedTransaction.date)}</span>
+            </Flex>
+            <Text fontFamily="monospace" fontSize="16px">
+              Notes
+            </Text>
+            <Input
+              placeholder="Add a note."
+              value={selectedTransaction.notes}
+              htmlSize={50} width='100' height='100'
+            />
+            <Flex alignItems="center">
+              <Icon as={GoPlus} boxSize={7} mr={2} />Add an attachment
+            </Flex>
+            <Text fontFamily="monospace" fontSize="16px">
+              Bank description
+            </Text>
+            <Text fontWeight="bold">{selectedTransaction.account}</Text>
+          </Stack>
         </ModalBody>
         <ModalFooter>
-          <Button colorScheme="blue" onClick={onClose}>
-            Close
-          </Button>
+          <Icon as={GoPaperclip} />
         </ModalFooter>
       </ModalContent>
     </Modal>
@@ -71,4 +116,3 @@ function TransactionDetail({ isOpen, onClose, selectedTransaction }: Transaction
 }
 
 export default TransactionDetail;
-
